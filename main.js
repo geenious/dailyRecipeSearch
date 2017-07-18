b1c61b9c590ac76cd780c1e5380829a161fc4df1
Jeff Irwin <jeff.irwin@live.com>
2017-07-18 14:24:44 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+let section = document.querySelector("#results");
+
+
+let searchButton = document.querySelector('#search');
+let queryInput = document.querySelector('#input');
+let url = "https://recipe-puppy-proxy.herokuapp.com/api/recipes?q=";
+
+searchButton.addEventListener('click', function() {
+  let val = queryInput.value;
+  let finalUrl = url + val;
+
+  fetch(finalUrl)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+
+    let html = "";
+    for (let i = 0; i < data.results.length; i++) {
+      html += `
+      <div>
+      <h3>${data.results[i].title}</h3>
+      <img src="${data.results[i].thumbnail}">
+      <p>Ingredients: ${data.results[i].ingredients}</p>
+      <p>Find more information <a href="${data.results[i].href}">here</a></p>
+      </div>`
+    }
+    section.innerHTML = html;
+  });
+});
